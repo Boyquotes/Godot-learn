0f9dc56b9b7fb92f33998c25e810e8ad22e52ad9
xmyhhh <xmycissy@outlook.com>
2022-06-08 19:33:38 +0800

    update

---
+++ script/demo_test/EnemyPig.gd
@@ -0,0 +1,16 @@
+extends Enemy
+
+class_name EnemyPigClass
+
+func _init(name).(name):
+
+	print("EnemyPigClass init")
+	pass
+
+func Attack(type):	
+
+	print("EnemyPig attack"+type)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
