be41884102f9abd81cb55565ec53e0ec4395ebc6
xmyhhh <xmycissy@outlook.com>
2022-06-21 18:07:39 +0800

    球坐标系

--- sceneList/demo21_camera/Scripts/CameraControl.gd
+++ sceneList/demo21_camera/Scripts/CameraControl.gd
@@ -1,3 +1,5 @@
+#CameraController V0.1 2022.6.21
+#Code By Xmy 
 extends Spatial
 
 enum CameraMode {Protagonist,FreeLook}
@@ -5,23 +7,239 @@ enum ProtagonistMode{Circle, Sphere}
 enum FreeLookMode{OnPlane}
 
 enum ProjectionMode{Perspective , Orthogonal}
+enum LerpMethod{Defalut}
+enum LookAtTargetType{SpatialNode,Vector3Point}
 
+enum CheckType{SetMode,SetSubMode,SetLookAtTargetType,SetLookAtSpatial,SetLookAtPoint,SetZenithAngelOffset,SetAzimuthAngelOffse,SetDistanceOffset,SetDistanceMax,SetDistanceMin,SetLerpSpeed}
 
-func _ready():
-	pass # Replace with function body.
+var config ={}
 
-
-func SetCamera(config:Dictionary):
-#	configDict.cameraMode=CameraOption.get_selected_id()
-#	configDict.lookAtObj= objects[LookAtOption.get_selected_id()]
-#	configDict.followObj = objects[FollowOption.get_selected_id()]
-#	configDict.projectionMode = ProjectionOption.get_selected_id()
+func _process(delta):
+	if(not config.has("cameraMode") or not config.has("cameraSubMode")):
+		return false
 	
-#	look_at(config.lookAtObj.transform.origin,Vector3.UP)
-#	LookAt(config.lookAtObj.transform.origin,Vector3.UP)
+	match config.cameraMode:
+		CameraMode.Protagonist:
+			ProtagonistProcess(delta)
+
+#region 外部方法
+func SetMode(mode):
+	if(not Check(mode,CheckType.SetMode)):
+		return
+	config.cameraMode = mode
+
+	#Set default "Common Config" if it is null
+	if(not config.has("isLerp")):
+		config.isLerp=true
+		config.lerpSpeed=0.5
+	if(not config.has("projectionMode")):
+		config.projectionMode=ProjectionMode.Perspective
+
+func SetSubMode(subMode):
+	if(not Check(subMode,CheckType.SetSubMode)):
+		return 
+	config.cameraSubMode=subMode
+	match subMode :
+			ProtagonistMode.Circle:
+				config.distanceMin = 15.0
+				config.distanceMax = 100.0
+				config.distanceOffset = 0.0
+
+				config.zenithAngelOffset = 0.0	
+				config.azimuthAngelOffset = 0.0	
+
+				config.isAutoSurround = false
+				config.autoSurroundSpeed = 30.0
+				config.lookAtTargetType = LookAtTargetType.Vector3Point
+
+func SetLookAtTargetType(value:int):
+	config.lookAtTargetType=value
+
+func SetLookAtSpatial(traget:Spatial):
+	if (not Check(traget,CheckType.SetLookAtSpatial)):
+		return
+
+	config.lookAtSpatial=traget
+
+func SetLookAtPoint(traget:Vector3):
+	if (not Check(traget,CheckType.SetLookAtPoint)):
+		return
+	config.lookAtPoint=traget
+
+func SetZenithAngelOffset(value:float):
+	if (not Check(value,CheckType.SetZenithAngelOffset)):
+		return
+	#TODO: 约束到01之间
+	config.zenithAngelOffset = value
+
+func SetAzimuthAngelOffse(value:float):
+	if (not Check(value,CheckType.SetAzimuthAngelOffse)):
+		return
+	#TODO: 约束到01之间
+	config.azimuthAngelOffset = value
+
+func SetDistanceOffset(value:float):
+	if (not Check(value,CheckType.SetDistanceOffset)):
+		return
+	#TODO: 约束到01之间
+	config.distanceOffset = value
+
+func SetDistanceMax(value:float):
+	if (not Check(value,CheckType.SetDistanceMax)):
+		return
+
+	config.distanceMax = value
+
+func SetDistanceMin(value:float):
+	if (not Check(value,CheckType.SetDistanceMin)):
+		return
+
+	config.distanceMin = value
+
+func SetLerp(value:bool):
+	config.isLerp=value
+
+func SetLerpSpeed(value:float):
+	if (not Check(value,CheckType.SetLerpSpeed)):
+		return
+
+	config.lerpSpeed=value
+#end 外部方法
+
+#region 内部方法(不要在外部调用这些方法)
+func ProtagonistProcess(delta):
+	if not config.has("lookAtSpatial") and not config.has("lookAtPoint"):
+		return
 	
-#	look_at_from_position(Vector3(2,2,2),config.lookAtObj.transform.origin,Vector3.UP)
-	LookAtPosition(Vector3(2,2,2),config.lookAtObj.transform.origin)
+	match config.cameraSubMode :
+		ProtagonistMode.Circle:
+			var distance = float((config.distanceMax-config.distanceMin)*config.distanceOffset)+config.distanceMin
+			var zenithAngel = config.zenithAngelOffset*3.14 #(极角)
+			var azimuthAngel = config.azimuthAngelOffset*2*3.14 #(方位角)
+
+			var targetPosition
+
+			if(config.lookAtTargetType==LookAtTargetType.SpatialNode):
+				targetPosition=config.lookAtSpatial.transform.origin
+			else:
+				targetPosition=config.lookAtPoint
+			
+			var cameraPosition = Vector3(targetPosition.x+distance*sin(zenithAngel)*cos(azimuthAngel),
+			targetPosition.y+distance*cos(zenithAngel),targetPosition.z+distance*sin(zenithAngel)*sin(azimuthAngel))
+
+
+			if(config.isLerp):
+				LookAtPositionLerp(cameraPosition,targetPosition)
+			else:
+				LookAtPosition(cameraPosition,targetPosition)
+
+func Check(input,checkType)->bool:
+	if(input == null):
+		print("CameraControllerError: set parameter can not be null")
+		return false
+	match checkType:
+
+		CheckType.SetMode:
+			#if new mode == current mode , just return
+			if(config.has("cameraMode") and config.cameraMode == input):
+				print("CameraControllerWarn: new mode == current mode, nothing changed!")
+				return false
+
+		CheckType.SetSubMode:
+			#if new subMode == current subMode , just return
+			if(config.has("cameraSubMode") and config.cameraSubMode == input):
+				print("CameraControllerWarn: new subMode == current subMode, nothing changed!")
+				return false
+
+		CheckType.SetLookAtTargetType:
+			if(not config.has("cameraMode")):
+				print("CameraControllerError: you have to set mode Protagonist first")
+				return false
+			elif(config.cameraMode !=CameraMode.Protagonist):
+				print("CameraControllerError: function SetLookAtTargetType() can be called only under mode Protagonist")
+				return false
+
+		CheckType.SetLookAtSpatial:
+			if(not config.has("cameraMode")):
+				print("CameraControllerError: you have to set mode Protagonist first")
+				return false
+			elif(config.cameraMode !=CameraMode.Protagonist):
+				print("CameraControllerError: function SetLookAtSpatial() can be called only under mode Protagonist")
+				return false
+			elif(config.lookAtTargetType==LookAtTargetType.Vector3Point):
+				print("CameraControllerError: function SetLookAtSpatial() can be called only under LookAtTargetType=SpatialNode")
+				return false
+
+		CheckType.SetLookAtPoint:
+				if(not config.has("cameraMode")):
+					print("CameraControllerError: you have to set mode Protagonist first")
+					return false
+				elif(config.cameraMode !=CameraMode.Protagonist):
+					print("CameraControllerError: function SetLookAtPoint() can be called only under mode Protagonist")
+					return false
+				elif(config.lookAtTargetType==LookAtTargetType.SpatialNode):
+					print("CameraControllerError: function SetLookAtSpatial() can be called only under LookAtTargetType=Vector3Point")
+					return false
+
+		CheckType.SetZenithAngelOffset:
+			if(not config.has("cameraMode")):
+				print("CameraControllerError: you have to set mode Protagonist first")
+				return false
+			elif(config.cameraMode !=CameraMode.Protagonist):
+				print("CameraControllerError: function SetZenithAngelOffset() can be called only under mode Protagonist")
+				return false
+
+		CheckType.SetAzimuthAngelOffse:
+			if(not config.has("cameraMode")):
+				print("CameraControllerError: you have to set mode Protagonist first")
+				return false
+			elif(config.cameraMode !=CameraMode.Protagonist):
+				print("CameraControllerError: function SetAzimuthAngelOffse() can be called only under mode Protagonist")
+				return false
+
+		CheckType.SetDistanceOffset:
+			if(not config.has("cameraMode")):
+				print("CameraControllerError: you have to set mode Protagonist first")
+				return false
+			elif(config.cameraMode !=CameraMode.Protagonist):
+				print("CameraControllerError: function SetSurroundAngelOffset() can be called only under mode Protagonist")
+				return false
+
+		CheckType.SetDistanceMax:
+			if(not config.has("cameraMode")):
+				print("CameraControllerError: you have to set mode Protagonist first")
+				return false
+			elif(config.cameraMode !=CameraMode.Protagonist):
+				print("CameraControllerError: function SetSurroundAngelOffset() can be called only under mode Protagonist")
+				return false
+			elif(input<0):
+				print("CameraControllerError: function SetDistanceMax(value) can be called when value >=0")
+				return false
+			elif(input<config.distanceMin):
+				print("CameraControllerError: fun50ction SetDistanceMax(value) can be called when DistanceMax >=DistanceMin")
+				return false
+
+		CheckType.SetDistanceMin:
+			if(not config.has("cameraMode")):
+				print("CameraControllerError: you have to set mode Protagonist first")
+				return false
+			elif(config.cameraMode !=CameraMode.Protagonist):
+				print("CameraControllerError: function SetSurroundAngelOffset() can be called only under mode Protagonist")
+				return false
+			elif(input<0):
+				print("CameraControllerError: function SetDistanceMin(value) can be called when value >=0")
+				return false
+			elif(input>config.distanceMax):
+				print("CameraControllerError: function SetDistanceMin(value) can be called when DistanceMax >=DistanceMin")
+				return false
+
+		CheckType.SetLerpSpeed:
+			if(input<=0 or input>1):
+				print("CameraControllerError: function SetLerpSpeed(value) can be called when 1	>=value >0")
+				return false
+
+
+	return true
 
 #不借助Spatial节点的look_at（）方法自己实现
 func LookAt(taregtPosition,worldUp=Vector3.UP):
@@ -33,7 +251,23 @@ func LookAt(taregtPosition,worldUp=Vector3.UP):
 	
 	transform.basis=Basis(cameraAxis_X,cameraAxis_Y,cameraAxis_Z)
 
+func LookAtLerp(taregtPosition,worldUp=Vector3.UP):
+	var cameraAxis_Z =(transform.origin-taregtPosition).normalized()
+
+	var cameraAxis_X= worldUp.cross(cameraAxis_Z).normalized()
+	
+	var cameraAxis_Y= cameraAxis_Z.cross(cameraAxis_X)
+	
+	var quat = Quat(transform.basis).slerp( Quat(Basis(cameraAxis_X,cameraAxis_Y,cameraAxis_Z)),config.lerpSpeed) 
+
+	transform.basis = Basis(quat)
+
 #不借助Spatial节点的look_at_from_position（）方法自己实现
 func LookAtPosition(position,taregtPosition,worldUp=Vector3.UP):
 	transform.origin=position
 	LookAt(taregtPosition,worldUp)
+
+func LookAtPositionLerp(position,taregtPosition,worldUp=Vector3.UP):
+	transform.origin= lerp(transform.origin,position,config.lerpSpeed)  
+	LookAtLerp(taregtPosition,worldUp)
+#end 内部方法
--- sceneList/demo21_camera/Scripts/Game.gd
+++ sceneList/demo21_camera/Scripts/Game.gd
@@ -6,26 +6,37 @@ var CameraControlScript=load("res://sceneList/demo21_camera/Scripts/CameraContro
 
 var cameraModeOption
 var cameraSubModeOption
-
-var lookAtOption
-var followOption
 var projectionOption
+
+var lookAtTargetTypeOption
+var lookAtSpatialOption
+var lookAtPointX
+var lookAtPointY
+var lookAtPointZ
+
+var lerpOption
+var lerpSwitch
+var lerpSpeed
 
 var distanceMax
 var distanceMin
 var distanceOffset
-var angelOffset
 
+var zenithAngelOffset
+var azimuthAngelOffset
 
 var cameraNode
 
 
-onready var HBoxLookAt=find_node("HBoxLookAt")
-onready var HBoxFollow=find_node("HBoxFollow")
-onready var HBoxDistance=find_node("HBoxDistance")
-onready var HBoxDistanceScroll=find_node("HBoxDistanceScroll")
-onready var HBoxAngel=find_node("HBoxAngel")
-onready var HBoxAutoRotate=find_node("HBoxAutoRotate")
+onready var HBoxLookAtSpatial = find_node("HBoxLookAtSpatial")
+onready var HBoxLookAtPoint = find_node("HBoxLookAtPoint")
+onready var HBoxFollow = find_node("HBoxFollow")
+onready var HBoxDistance = find_node("HBoxDistance")
+onready var HBoxDistanceScroll = find_node("HBoxDistanceScroll")
+onready var HBoxZenithAngel = find_node("HBoxZenithAngel")
+onready var HBoxAzimuthAngel = find_node("HBoxAzimuthAngel")
+onready var HBoxAutoRotate = find_node("HBoxAutoRotate")
+onready var HBoxCameraLerpMethod = find_node("HBoxCameraLerpMethod")
 
 
 func _ready():
@@ -33,7 +44,8 @@ func _ready():
 	cameraNode = find_node("Camera")
 	#Setp 2: 给Camera节点绑定脚本
 	cameraNode.set_script(CameraControlScript)
-	
+	cameraNode.set_process(true)
+	cameraNode.set_physics_process(true)
 	#Setp 3: 初始化UI界面中的按钮
 	InitButton()
 
@@ -46,38 +58,68 @@ func InitButton():
 	#Setp 2:绑定UI
 	cameraModeOption = find_node("HBoxCameraMode").get_node("OptionButton")
 	cameraSubModeOption = find_node("HBoxCameraSubMode").get_node("OptionButton")
-	
-	lookAtOption = find_node("HBoxLookAt").get_node("OptionButton")
-	followOption = find_node("HBoxFollow").get_node("OptionButton")
-	projectionOption = find_node("HBoxCameraProjection").get_node("OptionButton")
-	
-	distanceMax = find_node("HBoxDistance").get_node("TextEditMax")
-	distanceMin = find_node("HBoxDistance").get_node("TextEditMin")
-	distanceOffset = find_node("HBoxDistanceScroll").get_node("HScrollBar")
-	angelOffset = find_node("HBoxAngel").get_node("HScrollBar")
+	lookAtSpatialOption = find_node("HBoxLookAtSpatial").get_node("OptionButton")
+	lookAtTargetTypeOption = find_node("HBoxLookAtTargetType").get_node("OptionButton")
+	lookAtPointX =  find_node("HBoxLookAtPoint").get_node("x/TextEdit")
+	lookAtPointY =  find_node("HBoxLookAtPoint").get_node("y/TextEdit")
+	lookAtPointZ =  find_node("HBoxLookAtPoint").get_node("z/TextEdit")
 
+
+	projectionOption = find_node("HBoxCameraProjection").get_node("OptionButton")
+	lerpOption = find_node("HBoxCameraLerpMethod").get_node("OptionButton")
+
+	lerpSpeed = find_node("HBoxChangeLerp").get_node("Speed/TextEdit")
+	lerpSwitch =  find_node("HBoxChangeLerp").get_node("Swicth/Switch/CheckButton")
+
+	distanceMax = find_node("HBoxDistance").get_node("Max/TextEditMax")
+	distanceMin = find_node("HBoxDistance").get_node("Min/TextEditMin")
+	distanceOffset = find_node("HBoxDistanceScroll").get_node("CenterContainer2/HScrollBar")
+	
+	zenithAngelOffset = find_node("HBoxZenithAngel").get_node("CenterContainer2/HScrollBar")
+	azimuthAngelOffset = find_node("HBoxAzimuthAngel").get_node("CenterContainer2/HScrollBar")
+	
 	#Setp 3:绑定更新事件
 	cameraModeOption.connect("item_selected",self,"UpdateUILevel0")
 	cameraSubModeOption.connect("item_selected",self,"UpdateUILevel1")
-	
-	lookAtOption.connect("item_selected",self,"SetCameraController")
-	followOption.connect("item_selected",self,"SetCameraController")
+
+	lookAtSpatialOption.connect("item_selected",self,"UpdateLookAtSpatial")
+	lookAtTargetTypeOption.connect("item_selected",self,"UpdateLookAtTargetType")
+	lookAtPointX.connect("text_changed",self,"UpdateLookAtPoint")
+	lookAtPointY.connect("text_changed",self,"UpdateLookAtPoint")
+	lookAtPointZ.connect("text_changed",self,"UpdateLookAtPoint")
+
 	projectionOption.connect("item_selected",self,"SetCameraController")
+
+	lerpSpeed.connect("text_changed",self,"UpdateLerpSpeed")
+	lerpSwitch.connect("pressed",self,"UpdateLerpSwitch")
+
+	zenithAngelOffset.connect("value_changed",self,"UpdateZenithAngelOffset")
+	azimuthAngelOffset.connect("value_changed",self,"UpdateAzimuthAngelOffse")
 	
+	distanceOffset.connect("value_changed",self,"UpdateDistanceOffset")
 	
+	distanceMin.connect("text_changed",self,"UpdateDistanceMin")
+	distanceMax.connect("text_changed",self,"UpdateDistanceMax")
+
+	#Setp 4:选项框初始化选项
 	for object in objects:
-		lookAtOption.add_item("item : "+object.name)
-		followOption.add_item("item : "+object.name)
-	
+		lookAtSpatialOption.add_item("item : "+object.name)
+
 	for item in CameraControlScript.ProjectionMode:
 		projectionOption.add_item(item)
 		
 	for item in CameraControlScript.CameraMode:
 		cameraModeOption.add_item(item)
+	
+	for item in CameraControlScript.LerpMethod:
+		lerpOption.add_item(item)
+
+	for item in CameraControlScript.LookAtTargetType:
+		lookAtTargetTypeOption.add_item(item)
 
 	UpdateUILevel0(null)
 	
-func UpdateUILevel0(input):
+func UpdateUILevel0(input):  # mode change
 	match cameraModeOption.get_selected_id():
 		CameraControlScript.CameraMode.Protagonist:
 			cameraSubModeOption.clear()
@@ -88,63 +130,90 @@ func UpdateUILevel0(input):
 			cameraSubModeOption.clear()
 			for item in CameraControlScript.FreeLookMode:
 					cameraSubModeOption.add_item(item)
-					
+
+	#每次UI更新，都要更新相机脚本的参数
+	cameraNode.SetMode(cameraModeOption.get_selected_id())	
 	UpdateUILevel1(null)
 
-func UpdateUILevel1(input):
+
+func UpdateUILevel1(input):  # sub mode change
+
+	cameraNode.SetSubMode(cameraSubModeOption.get_selected_id())
+
 	match cameraModeOption.get_selected_id():
 		CameraControlScript.CameraMode.Protagonist:
 			UpdateUIProtagonist(null)
 
 		CameraControlScript.CameraMode.FreeLook:
 			UpdateUIFreeLook(null)
-	#每次UI更新，都要更新相机脚本的参数
-	SetCameraController(null)
+
 	
 func UpdateUIProtagonist(input):
-	HBoxLookAt.show()
-	HBoxFollow.show()
-	HBoxAngel.show()
+	UpdateLookAtTargetType(null)
+
+	HBoxAzimuthAngel.show()
+	HBoxZenithAngel.show()
 	HBoxAutoRotate.show()
+
+	UpdateLookAtSpatial(null)
 	match cameraSubModeOption.get_selected_id():
-		
 		CameraControlScript.ProtagonistMode.Circle:
 			HBoxDistance.show()
 			HBoxDistanceScroll.show()
-			
+			UpdateZenithAngelOffset(null)
+			UpdateAzimuthAngelOffse(null)
+			UpdateDistanceOffset(null)
+			UpdateDistanceMin()
+			UpdateDistanceMax()
+			UpdateLerpSpeed()
 		CameraControlScript.ProtagonistMode.Sphere:
 			HBoxDistance.hide()
 			HBoxDistanceScroll.hide()
 
-
 func UpdateUIFreeLook(input):
-	HBoxLookAt.hide()
-	HBoxFollow.hide()
+	HBoxLookAtSpatial.hide()
 
-	
-func SetCameraController(input):
-	#Setp 1: 从UI读取参数，准备设置参数
-	var configDict = ConfigBuilder()
+func UpdateLookAtSpatial(input):
+	cameraNode.SetLookAtSpatial(objects[lookAtSpatialOption.get_selected_id()])
 
-	#Setp 2: 配置相机脚本
-	cameraNode.SetCamera(configDict)
+func UpdateLookAtPoint():
+	var point=Vector3(float(lookAtPointX.text),float(lookAtPointY.text),float(lookAtPointZ.text))
+	cameraNode.SetLookAtPoint(point)
 
-func ConfigBuilder():
-	var configDict={}
-	#Mode
-	configDict.cameraMode=cameraModeOption.get_selected_id()
-	configDict.cameraSubMode=cameraSubModeOption.get_selected_id()
-	#Common
-	configDict.projectionMode=projectionOption.get_selected_id()
-	configDict.cameraSubMode=cameraSubModeOption.get_selected_id()
-	match cameraModeOption.get_selected_id():
-		CameraControlScript.CameraMode.Protagonist:
+func UpdateZenithAngelOffset(input):
+	cameraNode.SetZenithAngelOffset(zenithAngelOffset.value)
 
-			configDict.lookAtObj= objects[lookAtOption.get_selected_id()]
-			configDict.followObj = objects[followOption.get_selected_id()]
-			configDict.projectionMode = projectionOption.get_selected_id()
-			pass
-		CameraControlScript.CameraMode.FreeLook:
-			pass
-	return configDict
-			
+func UpdateAzimuthAngelOffse(input):
+	cameraNode.SetAzimuthAngelOffse(azimuthAngelOffset.value)
+
+func UpdateDistanceOffset(input):
+	cameraNode.SetDistanceOffset(distanceOffset.value)
+
+func UpdateDistanceMin():
+	cameraNode.SetDistanceMin(float(distanceMin.text))
+
+func UpdateDistanceMax():
+	cameraNode.SetDistanceMax(float(distanceMax.text))
+
+func UpdateLerpSwitch():
+	var toggleState = lerpSwitch.is_pressed()
+	if(toggleState):
+		HBoxCameraLerpMethod.show()
+	else:
+		HBoxCameraLerpMethod.hide()
+	cameraNode.SetLerp(toggleState)
+
+func UpdateLerpSpeed():
+	cameraNode.SetLerpSpeed(float(lerpSpeed.text))
+
+func UpdateLookAtTargetType(input):
+	match lookAtTargetTypeOption.get_selected_id():
+		CameraControlScript.LookAtTargetType.SpatialNode:
+			HBoxLookAtPoint.hide()
+			HBoxLookAtSpatial.show()
+			cameraNode.SetLookAtTargetType(CameraControlScript.LookAtTargetType.SpatialNode)
+		CameraControlScript.LookAtTargetType.Vector3Point:
+			HBoxLookAtPoint.show()
+			HBoxLookAtSpatial.hide()
+			cameraNode.SetLookAtTargetType(CameraControlScript.LookAtTargetType.Vector3Point)
+			UpdateLookAtPoint()
